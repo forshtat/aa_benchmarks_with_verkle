18462715b7cd05d837fbc176066a482b85afb120
Alex Forshtat <forshtat1@gmail.com>
2024-04-12 16:49:09 +0200

    Use 'eslint'

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from 'globals'
+// import tseslint from 'typescript-eslint'
+
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginJs from '@eslint/js'
+
+// mimic CommonJS variables -- not needed if using CommonJS
+// eslint-disable-next-line
+const __filename = fileURLToPath(import.meta.url)
+// eslint-disable-next-line
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
+
+export default [
+  { files: ['test/**/*.ts'] },
+  { languageOptions: { globals: globals.node } },
+  ...compat.extends('standard-with-typescript')
+  // ...tseslint.configs.recommended
+]
